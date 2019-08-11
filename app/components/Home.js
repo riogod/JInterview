a6a57b87428623623139c3e37e20aae80ba70377
Vyacheslav Krasnyansky <riogod@gmail.com>
2019-08-11 10:56:31 +0300

    added header module

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
+import Header from './header/Header';
 
 type Props = {};
 
@@ -12,6 +13,7 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div className={styles.container} data-tid="container">
+        <Header />
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
