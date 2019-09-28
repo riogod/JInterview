403785c1e3ae3891bcef1b211ef0f2de855455dc
riogod <riogod@gmail.com>
2019-09-28 11:41:08 +0300

    addede some missing files

---
+++ app/db/models/categories.js
@@ -0,0 +1,29 @@
+// import { Model } from 'sequelize';
+
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const appCategories = db.define(
+  'categories',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    parent_id: Sequelize.INTEGER,
+    category_name: Sequelize.STRING,
+    category_descr: Sequelize.STRING,
+    sort_order: Sequelize.INTEGER
+  },
+  {
+    timestamps: false,
+    getterMethods: {
+      getValue() {
+        return this.getDataValue('category_name');
+      }
+    }
+  }
+);
+
+module.exports = appCategories;
